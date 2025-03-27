c12b9a00171e3ae3c7eebac287bd313c5743b6b0
Nishidh-ins-47 <nishidhsingh97@gmail.com>
2025-03-27 15:52:12 +0530

    SQLite Migrations: Insert, Update, and Delete

--- day-16/day16/myapp/urls.py
+++ day-16/day16/myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import student_list, add_student
+from .views import student_list, add_student, update_student, delete_student
 
 urlpatterns = [
     path('', student_list, name='student_list'),  # Home page with student list
     path('add/', add_student, name='add_student'),  # Form to add student
+    path('update/<int:student_id>/', update_student, name='update_student'),
+    path('delete/<int:student_id>/', delete_student, name='delete_student'),
 ]
--- day-16/day16/myapp/views.py
+++ day-16/day16/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
 from .forms import StudentForm
 from django.urls import reverse
@@ -17,3 +17,22 @@ def add_student(request):
         form = StudentForm()
 
     return render(request, 'add_student.html', {'form': form})
+
+
+def update_student(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('student_list'))  # Redirect to list after updating
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'update_student.html', {'form': form})
+
+def delete_student(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    if request.method == "POST":
+        student.delete()
+        return redirect(reverse('student_list'))  # Redirect to list after deletion
+    return render(request, 'delete_student.html', {'student': student})
